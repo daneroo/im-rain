8f2f5c9b5b050fef103084eabe10e8eff55f3c42
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-11-11 16:51:38 -0500

    moveed from cradle to nano for couch access

--- master.js
+++ master.js
@@ -1,68 +1,80 @@
 var _  = require('underscore');
 _.mixin(require('underscore.string'));
 var os = require('os');
-var cradle = require('cradle');
+var nano = require('nano');
 var follow = require('follow');
-var dbopts = {
-  host:'127.0.0.1',
-  port:5984,
-  auth:{
-    username:'daniel',
-    password:'xxxxxx'
-  }
-};
-var conn = new (cradle.Connection)(dbopts);
+
+var authdburl = "http://daniel:pokpok@127.0.0.1:5984";
+var conn = nano(authdburl);
 
 function Monitor(dbname){
   this.dbname = dbname;
-  this.db = conn.database(dbname);
+  this.db = conn.use(dbname);
+  this.createAttempts=3;
 }
 Monitor.prototype = {
   ping: function (){
     var self=this;
     var hostkey='heartbeat:'+os.hostname()+':'+self.dbname;
-    self.db.save(hostkey, {
-      stamp: new Date(),
-      host: os.hostname(),
-      pid: process.pid
-    }, function (err, res) {
-      if (err) {
-        console.log('error',err)
+    self.db.get(hostkey,function(e,doc,h){
+      if (e){
+        console.log('err:get',e);
+        var doc = {
+          stamp: new Date(),
+          host: os.hostname(),
+          pid: process.pid
+        };
+        self.db.insert(doc,hostkey, function (error, body, headers) {
+          if (error) {
+            console.log('error',error)
+            if(error.message === 'no_db_file' && self.createAttempts>0) {
+              self.createAttempts--;
+              console.log('create:'+self.dbname+':'+self.createAttempts);
+              // create database and retry
+              return conn.db.create(self.dbname, function () {
+                self.ping();
+              });
+            }
+          } else {
+            console.log('insert pingd:'+self.dbname+':'+body);
+          }
+        });  
       } else {
-        console.log('pingd:'+self.dbname+':'+res);
-      }
-    });  
-  },
-  createAndPing: function(){
-    var self=this;
-    this.db.exists(function (err, exists) {
-      if (err) {
-        console.log('error', err);
-      } else if (!exists) {
-        console.log('creating database');
-        self.db.create();
-      }
-      //self.ping();
-      setInterval(function(){self.ping()},10000);
+        //console.log('got',doc);
+        doc.stamp=new Date();
+        doc.host= os.hostname();
+        doc.pid= process.pid;        
+        self.db.insert(doc,hostkey, function (error, body, headers) {
+          if (error) {
+            console.log('error',error)
+          } else {
+            console.log('update pingd:'+self.dbname+':',body.id||'no-id',body.rev||'no-rev');
+          }
+        });
+      }      
     });
+    return;
+    
+  },
+  start: function(){
+    var self=this;
+    self.ping();
+    setInterval(function(){self.ping()},10000);
   }
 };
 
 /* 
 */
-new Monitor('rain-0').createAndPing();
-new Monitor('rain-1').createAndPing();
+new Monitor('rain-0').start();
+new Monitor('rain-1').start();
 
-console.log({
-  'config':conn.config(),
-  'databases':conn.databases()
-});
 
 function track(dbname){
-  follow({db:"http://daniel:xxxxxx@127.0.0.1:5984/"+dbname, include_docs:true}, function(error, change) {
+  follow({db:"http://daniel:pokpok@127.0.0.1:5984/"+dbname, include_docs:true}, function(error, change) {
     if(!error) {
-      console.log(dbname+"::change " + change.seq + " has " + Object.keys(change.doc).length + " fields");
-      console.log(dbname+'::change',change);
+      //console.log(dbname+"::change " + change.seq + " has " + Object.keys(change.doc).length + " fields");
+      console.log(dbname+"::change " + change.seq + " id:" + change.doc._id||'no-id');
+      //console.log(dbname+'::change',change);
     } else {
       console.log('follow::error',dbname,error);
     }
@@ -71,3 +83,18 @@ function track(dbname){
 
 track('rain-0');
 track('rain-1');
+var continuous=true;
+conn.db.replicate('rain-0','rain-1',continuous,function(e,r,h){
+  if (e){
+    console.log('repl:err',e);
+  } else {
+    console.log('repl:r,h',r,h);
+  }
+});
+conn.db.replicate('rain-1','rain-0',continuous,function(e,r,h){
+  if (e){
+    console.log('repl:err',e);
+  } else {
+    console.log('repl:r,h',r,h);
+  }
+});
