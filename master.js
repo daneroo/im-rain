1944c7c484fe1a75a611df7a949c2fca51688196
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-07-18 04:00:12 -0400

    more refactoring

--- master.js
+++ master.js
@@ -5,10 +5,12 @@ var _ = require('underscore');
 _.mixin(require('underscore.string'));
 var nano = require('nano');
 var Pulse = require('./lib/pulse').Pulse;
+var log = require('./lib/log');
+var config = require('./lib/config');
 var auth = require('./lib/auth');
+var replicate = require('./lib/replicate');
+var db = require('./lib/db');
 
-//  for logging
-var log = console;
 
 var rains = []; //{srv,db}
 
@@ -21,78 +23,46 @@ auth.setup()
 
     _.times(2, function() {
       rains.push({
-        srv: auth.couchUrl(),
+        srv: config.couch.url,
         db: 'rain-' + counter++
       });
     });
 
+    var replicator = true;
+    if (replicator) {
+      one();
+      setInterval(one, 30000);
+    }
+
+    var heartbeat = false;
+    if (heartbeat) {
+      rains.forEach(function(r, i, ary) {
+        var rain = new Pulse(r.srv, r.db).start();
+        // rain.track(); // this works
+        ary[i].rain = rain;
+      });
+    }
     // start the pulse for local databases
-    rains.forEach(function(r, i, ary) {
-      var rain = new Pulse(r.srv, r.db).start();
-      // rain.track(); // this works
-      ary[i].rain = rain;
-    });
-
-    // for replication later
-    // var servers = ["dirac.imetrical.com", "darwin.imetrical.com"];
-    rains.push({
-      srv: 'https://daniel:couchone42@imetrical.iriscouch.com',
-      db: 'rain-cloud'
-    });
-
   })
   .catch(function(err) {
     log.error('auth:setup error');
     throw new Error('Unable to setup authentication');
   });
 
-function replicatorRing() {
-  rains.forEach(function(r, i, ary) {
-    var ip1 = (i + 1) % ary.length;
-    var next = ary[ip1];
-    var replsrv = r.srv;
-    if (r.srv.match(/iriscouch/)) {
-      console.log('iriscouch swap server, use:', next.srv);
-      replsrv = next.srv;
-    }
-
-    var desiredSource = (replsrv === r.srv) ? r.db : r.srv + '/' + r.db;
-    var desiredTarget = (replsrv === next.srv) ? next.db : next.srv + '/' + next.db;
-
-    console.log('repl', desiredSource, desiredTarget);
-
-    // use nano to replicate (_active_tasks)
-    nano(replsrv).db.replicate(desiredSource, desiredTarget, {
-      id: 'zabaza' + Math.random(),
-      continuous: true
-    }, function(err, resp, headers) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('+repl:', replsrv, desiredSource, '-->', desiredTarget);
-      }
+function one() {
+  // return replicate.createDBs(rains).then
+  return Promise.resolve(rains)
+    .then(function() {
+      return db.activeTasks(rains[0]);
+    })
+    .then(function() {
+      replicate.replicatorRing(rains);
+    })
+    .then(function() {
+      return db.activeTasks(rains[0]);
+    })
+    .catch(function(error) {
+      log.error(error);
+      // pretty fatal..
     });
-
-    console.log('All done replicating');
-    // return;    // below uses replicator db.
-
-    nano(replsrv).relax({
-      path: '_active_tasks'
-    }, function(err, tasks) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      // console.log(tasks);
-      tasks.forEach(function(task) {
-        console.log('task:', task.type, task.source, task.target, task.replication_id);
-      });
-    });
-  });
-}
-
-var replicator = true;
-if (replicator) {
-  replicatorRing();
-  setInterval(replicatorRing, 30000);
 }
